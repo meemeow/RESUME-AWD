61dad663e158a30e3262f3ab88d3bf42c85479a1
meemeow <155798471+meemeow@users.noreply.github.com>
2024-03-16 14:19:22 +0800

    Delete JS directory

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('togBtn').addEventListener('change', function(event) {
-    document.body.classList.toggle('night-mode', event.target.checked);
-  });
-
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WWPBC2JY9X');
